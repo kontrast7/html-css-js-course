dc9521f71ee3793995ac698cac76cd9e6866306d
kontrast7 <maks.mashko@gmail.com>
2021-05-04 18:15:25 +0300

    fix delete

--- 07/kontrast7/js/script.js
+++ 07/kontrast7/js/script.js
@@ -1,91 +1,108 @@
-// TODO
-const todoItems = document.getElementById('todo-items');
-const btnTodo = document.getElementById('todo-submit');
-const btnAdd = document.getElementById('todo-add');
-const review = document.getElementById('review');
-const email = document.getElementById('email');
-const name = document.getElementById('name');
+// TODOLIST
+const todoItems = document.getElementById("todo-items");
+const btnTodo = document.getElementById("todo-submit");
+
+const review = document.getElementById("review");
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+
+const form = document.getElementsByTagName("form");
+const url = "https://quiet-spire-94328.herokuapp.com/reviews";
 
 function clear() {
-    review.value = "";
-    email.value = "";
-    name.value = "";
+  review.value = "";
+  email.value = "";
+  name.value = "";
+  todoItems.innerHTML = "";
 }
 
-function add(e) {
-    e.preventDefault();
-    const div = document.createElement('div');
-    const closeDiv = document.createElement('button');
-    closeDiv.classList.add('closeVisible');
+function loadingTodoList() {
+  clear();
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      let reviews = json.reviews;
+      reviews.forEach((element) => {
+        const div = document.createElement("div");
+        const closeDiv = document.createElement("button");
+        closeDiv.classList.add("closeVisible");
 
-    div.classList.add('item');
-    closeDiv.innerHTML = 'Delete';
+        div.classList.add("item");
+        closeDiv.innerHTML = "Delete";
 
-    const count = document.querySelectorAll('.item').length;
-    div.innerHTML = '<div>Name: ' + name.value  + '</div>Email: ' + '<div>' + email.value + '</div>Review: ' + '<div>' + review.value  + '</div>';
+        const count = document.querySelectorAll(".item").length;
+        const countClose = document.querySelectorAll(".closeVisible").length;
+        console.log(countClose);
+
+        div.innerHTML =
+          "<div>Name: " +
+          element.name +
+          "</div>Email: " +
+          "<div>" +
+          element.email +
+          "</div>Review: " +
+          "<div>" +
+          element.review +
+          "</div>";
+
+        div.id = `id${count}`;
+        closeDiv.id = `close${count}`;
+
+        todoItems.append(div);
+        div.append(closeDiv);
+        deleteTodo(element.id)
+      });
+    })
+    .catch(() => {
+      console.log(ERROR);
+    });
+}
+
+loadingTodoList();
+
+btnTodo.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      review: review.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+      loadingTodoList();
+    });
+});
 
 
-    div.id = `id${count}`;
-    closeDiv.id = `close${count}`
-
-    todoItems.append(div);
-    div.append(closeDiv);
-
-
-    for (let i = 0; i <= count; i++) {
-        let els = document.querySelectorAll(".item")[i];
-        let els2 = document.querySelectorAll(".closeVisible")[i];
-        els2.addEventListener('click', () => {
-            els.remove();
-
+function deleteTodo(id) {
+  const deleteBtn = document.querySelectorAll(".closeVisible");
+  deleteBtn.forEach((item) =>
+    item.addEventListener("click", () => {
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify({ id: id }),
+      })
+        .then((response) => {
+          return response.json();
         })
-    }
-
-};
-
-btnAdd.addEventListener('click', add);
-
-btnTodo.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    // _______________________POST
-    const url = 'https://quiet-spire-94328.herokuapp.com/reviews';
-    const j = {
-        email: email.value,
-        id: "",
-        name: name.value,
-        review: review.value
-    };
-
-    fetch(url, {
-            method: 'post',
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            },
-            body: JSON.stringify(j) // Упаковка
-
-        }).then(response => response.json()) // Ответ с сервера переводится в JSON
-        .then(res => console.log(res))
-        .catch(function (err) {
-
+        .then((data) => {
+          console.log(data);
+          loadingTodoList();
+        })
+        .catch(function (error) {
+          console.log(error);
         });
-
-
-    // _______________________GET
-    async function getData(url) {
-        const res = await fetch(url);
-        const json = await res.json();
-        return json;
-    }
-
-    getData(url)
-        .then((data) => renderList(data));
-
-    function renderList(data) {
-        let obj = data;
-        for (key in obj) {
-            console.log(obj[key]);
-        }
-    }
-    clear();
-})
+    })
+  );
+}
